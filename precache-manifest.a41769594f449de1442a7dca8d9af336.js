a5b1a7ce97a621b504e19e7db2df2bd718f71e0e
Sangeet Subedi <sangeetsub@Sangeets-MacBook-Pro.local>
2020-06-25 19:02:33 -0500

    Updates

---
+++ precache-manifest.a41769594f449de1442a7dca8d9af336.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "522edaabeeb08d6a09c73318be8b2768",
+    "url": "/index.html"
+  },
+  {
+    "revision": "dd803de667edc05f03e1",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "bc6fdd786b6fbc563426",
+    "url": "/static/js/2.eb9097fd.chunk.js"
+  },
+  {
+    "revision": "3877e6dd129d25248c4a588c1baff1a0",
+    "url": "/static/js/2.eb9097fd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dd803de667edc05f03e1",
+    "url": "/static/js/main.de233e0e.chunk.js"
+  },
+  {
+    "revision": "baa2b955c5eeaf5fc216",
+    "url": "/static/js/runtime-main.2f035890.js"
+  }
+]);
